ed62a702851e2a6933987b1c0aea544b360c8a33
tvio <tvio@centrum.cz>
2023-12-09 19:12:55 +0100

    prvni verze, generuje jidlo od ocasu

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # zkusim primo pres spiretes classy, app  a dalsi
 import pygame
 import random
+import time
 from array import *
 
 pygame.init()
-speed=4
-height=60
-width=60
+height=30
+width=30
 windowW = 840
 windowH = 600
 white = (255,255,255)
 red = (255,0,0)
+blue = pygame.Color(0, 0, 255)
+green = pygame.Color(0, 255, 0)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
 pygame.display.set_caption('Had')
 windowClock = pygame.time.Clock()
 window = pygame.display.set_mode((windowW,windowH))
@@ -18,8 +22,8 @@ window = pygame.display.set_mode((windowW,windowH))
 
 class grid():
    def __init__(self):
-    self.sizex = windowW /width
-    self.sizey = windowH/height 
+    self.sizex = windowW /width-1
+    self.sizey = windowH/height-1 
     self.gridx = 0
     self.gridy = 0
    def transform(self,kam,x,y):
@@ -71,14 +75,15 @@ class Player(object):
           self.color = color
           self.delkaVlaku = 1
           self.vagony = []
+          self.hgridx = gridx
+          self.hgridy = gridy-1
     def draw(self,gridx,gridy,smer):
         self.gridx = gridx
         self.gridy = gridy
         grid.draw(self,self.gridx,self.gridy,self.color)
            
     
-    def pridejVagon(self):
-       self.delkaVlaku +=1 
+     
 class Jidlo(Player):
     def __init__(self,gridx,gridy,color):
              super().__init__(gridx,gridy,color) 
@@ -89,31 +94,25 @@ class Jidlo(Player):
        #pygame.draw.rect(window,self.color,pygame.Rect(self.x,self.y,width,height))
         grid.draw(self,self.gridx,self.gridy, self.color) 
 class Vagon(Jidlo):
-   def __init__(self,gridx,gridy,cislo):
-    super().__init__(gridx,gridy,cislo)
+   def __init__(self,cislo):
+    #super().__init__(cislo)
     self.cislo = cislo
-    self.gridx = gridx
-    self.gridy = gridy
-    self.hgridx = gridx
-    self.hgridy = gridy
+    self.gridx = 900
+    self.gridy = 900
+    self.hgridx = 900
+    self.hgridy = 900
    def nastavSouradnice(self,gridx,gridy,smer):
-      
       self.gridx = gridx
-      self.hgridx =  gridx
       self.gridy = gridy 
-      self.hgridy= gridy
       if smer=='left':
          self.gridx+=1
-         self.hgridx+=2
       elif smer=='right':
          self.gridx-=1 
-         self.hgridy+=2       
       elif smer=='up':
-         self.gridy+=1 
-         self.hgridy+=2       
+         self.gridy+=1         
       elif smer=='down':
          self.gridy-=1   
-         self.hgridy-=2      
+             
       
    def draw(self):
       grid.draw(self,self.gridx,self.gridy,red)
@@ -124,16 +123,10 @@ class Vagon(Jidlo):
       self.gridy = gridy
 class Run(object):
     stopped = False
-    #x=0
-    #y=0
-    #presun na gird ze souracnic 1..size
-    gx=0
-    gy=0
-    hgx=0
-    hgy=0
     height=60
     width=60
     smer='down'
+    speed=7
     def __init__(self):
         self.Main()
     def nastavSmer(self,klavesa):
@@ -152,16 +145,36 @@ class Run(object):
         random.shuffle(rgbl)
         while rgbl == [255,255,255]:
          random.shuffle(rgbl)
-        return tuple(rgbl)
+        #return tuple(rgbl)
+        return random.choices(range(256), k=3)
+    def game_over(self):
+       my_font = pygame.font.SysFont('times new roman', 50)
+       text = my_font.render('Konec',True,blue)
+       game_over_rect = text.get_rect()
+       game_over_rect.midtop = (windowW/2, windowH/4)
+       window.blit(text, game_over_rect)
+       pygame.display.flip()
+       time.sleep(5)
+       pygame.quit()
+       quit()
     def Main(self):
         stopped=False
         g = grid()
-        vagonCislo = 0
+        vagonPocet = 0
+        uvodniDelka = 2
         vagony = []
-        player1 = Player(0,0,red)
+        player1 = Player(0,2,red)
+        for i in (range (uvodniDelka)):
+           vagonPocet+=1
+           vagony.append( Vagon (vagonPocet))
+           if vagonPocet==1:
+            vagony[i].nastavSouradnice(player1.gridx,player1.gridy,Run.smer)
+           else:
+            vagony[i].nastavSouradnice(vagony[i-1].gridx,vagony[i-1].gridy,Run.smer)
+        
         jidlo = Jidlo(random.randint(0,g.sizex),random.randint(0,g.sizey),self.random_color())
-
-
+        wait = False
+        pocetCekani = 0
         while stopped == False:
          window.fill(white)
          for event in pygame.event.get():
@@ -178,45 +191,52 @@ class Run(object):
                if event.key == pygame.K_DOWN : 
                   self.nastavSmer('down')
                     
-         if jidlo.kontrolaKolize(Run.gx,Run.gy)==1:
-            player1.pridejVagon()
-            vagonCislo+=1
-            vagony.append( Vagon (Run.gx,Run.gy,vagonCislo))
-            if vagonCislo == 1 :
-            #pole vagonu zacina na 0 , odecist jedna od cislaVagonu
-               vagony[0].nastavSouradnice(Run.gx,Run.gy,Run.smer)
-            else:
-               vagony[vagonCislo-1].nastavSouradnice(vagony[vagonCislo-2].gridx,vagony[vagonCislo-2].gridy,Run.smer)
-           
-            
-             
+         if jidlo.kontrolaKolize(player1.gridx,player1.gridy)==1:
+            vagonPocet+=1
+            vagony.append( Vagon (vagonPocet))
+            Run.speed+=0.5
+            print(Run.speed)
             window.fill(red)
-          
             del jidlo
             jidlo = Jidlo(random.randint(0,g.sizex),random.randint(0,g.sizey),self.random_color())
+            if vagonPocet == 1 :
+               vagony[0].gridx=player1.gridx
+               vagony[0].gridy=player1.gridy
+               vagony[0].hgridx=player1.hgridx
+               vagony[0].hgridy=player1.hgridy
+            else:
+             wait = True
+             pocetCekani = vagonPocet
          else:
-            window.fill(white)
-         
-         
-         
-         Run.gx,Run.gy,Run.hgx,Run.hgy = g.automove(Run.smer,Run.gx,Run.gy)
-         player1.draw(Run.gx,Run.gy,Run.smer) 
-
-         for i in range(0,len(vagony)):
+             window.fill(white)
+         if wait:
+          pocetCekani-=1
+          if pocetCekani==0:
             
-            if i == 0:
-              vagony[i].gridx = Run.hgx
-              vagony[i].gridy = Run.hgy
-            else: 
-              vagony[i].aktualizujSouradnice(vagony[i-1].hgridx,vagony[i-1].hgridy)
-              
-         for i in range(0,len(vagony)):
-          vagony[i].draw()
+            vagony[vagonPocet-1].gridx=vagony[len(vagony)-2].gridx
+            vagony[vagonPocet-1].gridy=vagony[len(vagony)-2].gridy
+            wait = False
          
+         #check crash
+         for i in range(len(vagony)):
+            if vagony[i].gridx  == player1.gridx and vagony[i].gridy == player1.gridy:
+               self.game_over() 
+         
+         player1.draw(player1.gridx,player1.gridy,Run.smer) 
+         for i in range(len(vagony)):
+          vagony[i].draw()            
        
          jidlo.drawJidlo()
          pygame.display.update()
-         windowClock.tick(speed)
+         player1.gridx,player1.gridy,player1.hgridx,player1.hgridy = g.automove(Run.smer,player1.gridx,player1.gridy)
+         for i in reversed(range(len(vagony))):
+            if i == 0:
+              vagony[i].gridx = player1.hgridx
+              vagony[i].gridy = player1.hgridy
+            else: 
+              vagony[i].aktualizujSouradnice(vagony[i-1].gridx,vagony[i-1].gridy)
+
+         windowClock.tick(Run.speed)
         
 
 Run()
